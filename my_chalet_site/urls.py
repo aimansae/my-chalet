700360c377edd62325dfaf9c613b6348343e57ab
aimansae <saeedaimann@gmail.com>
2022-11-03 19:27:53 +0000

    Add styling to home.html, create description.html and view

--- my_chalet_site/urls.py
+++ my_chalet_site/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('description/<chalet_id>',
+         views.ChooseChalet.as_view(), name="description"),
 ]
--- my_chalet_site/views.py
+++ my_chalet_site/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
+from django.views.generic import ListView, View
 from .models import ChaletList, SelectChalet
+# from django.views.generic import View
 # Create your views here.
 
 
@@ -10,3 +12,12 @@ def home(request):
         'chalet_list': chalet_list
     }
     return render(request, 'home.html', context)
+
+
+class ChooseChalet(View):
+    '''show selected chalet view'''
+def get(self, request, route_id, *args, **kwargs):
+    queryset = SelectChalet.objects.all()
+    chalet = get_object_or_404(queryset, id=chalet_id)
+
+    return render(request, 'description.html')
