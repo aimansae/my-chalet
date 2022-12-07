b778009e9a99887376a812fedb15ae41f4d10e7c
aimansae <saeedaimann@gmail.com>
2022-12-07 18:23:25 +0000

    add https://my-chalet.herokuapp.com to csrf trused origins

--- my_chalet/settings.py
+++ my_chalet/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['my-chalet.herokuapp.com', 'localhost']
-CSRF_TRUSTED_ORIGINS = ['https://8000-aimansae-mychalet-cmwev90wvp8.ws-eu77.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://8000-aimansae-mychalet-cmwev90wvp8.ws-eu77.gitpod.io', 'https://my-chalet.herokuapp.com']
+
 
 # Application definition
 
