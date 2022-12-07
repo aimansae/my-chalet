7473c89a77a971270c6375d7cde03d0e35acaed3
aimansae <saeedaimann@gmail.com>
2022-12-07 18:38:23 +0000

    Modify allowed hosts with * to resolve error 400

--- my_chalet/settings.py
+++ my_chalet/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['my-chalet.herokuapp.com', 'localhost']
+#ALLOWED_HOSTS = ['my-chalet.herokuapp.com', 'localhost', '127.0.0.1']
+
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://8000-aimansae-mychalet-cmwev90wvp8.ws-eu77.gitpod.io','https://my-chalet.herokuapp.com']
 
 
